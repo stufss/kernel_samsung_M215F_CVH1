6ce1dd9bac101aaff67cf43816f19d6ceebe70f6
Yillié <yilliee@protonmail.com>
2022-06-18 05:09:53 +0500

    kernel : ptrace : Remove all entries of five_ptrace
    Signed-off-by: Yillié <yilliee@protonmail.com>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -1143,7 +1143,6 @@ SYSCALL_DEFINE4(ptrace, long, request, long, pid, unsigned long, addr,
 	long ret;
 
 	if (request == PTRACE_TRACEME) {
-		five_ptrace(current, request);
 		ret = ptrace_traceme();
 		if (!ret)
 			arch_ptrace_attach(current);
@@ -1156,7 +1155,6 @@ SYSCALL_DEFINE4(ptrace, long, request, long, pid, unsigned long, addr,
 		goto out;
 	}
 
-	five_ptrace(child, request);
 
 	if (request == PTRACE_ATTACH || request == PTRACE_SEIZE) {
 		ret = ptrace_attach(child, request, addr, data);
@@ -1295,7 +1293,6 @@ COMPAT_SYSCALL_DEFINE4(ptrace, compat_long_t, request, compat_long_t, pid,
 	long ret;
 
 	if (request == PTRACE_TRACEME) {
-		five_ptrace(current, request);
 		ret = ptrace_traceme();
 		goto out;
 	}
@@ -1306,7 +1303,6 @@ COMPAT_SYSCALL_DEFINE4(ptrace, compat_long_t, request, compat_long_t, pid,
 		goto out;
 	}
 
-	five_ptrace(child, request);
 
 	if (request == PTRACE_ATTACH || request == PTRACE_SEIZE) {
 		ret = ptrace_attach(child, request, addr, data);
