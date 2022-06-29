efbc25f963eb2fed100f6e917f8927f32f15cfa2
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-06-29 18:33:47 +0530

    mm: Import memory management hacks from Oplus
    This imports a couple of memory management hacks used by Oplus (Oppo, Realme, and OnePlus) on their recent devices. This patch does the following:
    
    - Add a separate swappiness (default 60) value to use if task isn't handled by kswapd
    - Don't throttle tasks with OOM value < 0
    - Increase zsmalloc's maximum page order
    
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    [TenSeventy7: Make the Kconfig more descriptive]
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -131,6 +131,9 @@ static int one_hundred = 100;
 #ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
 static int max_swappiness = 300;
 #endif
+#ifdef CONFIG_OPLUS_MM_HACKS
+extern int direct_vm_swappiness;
+#endif /* CONFIG_OPLUS_MM_HACKS */
 static int one_thousand = 1000;
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
@@ -1425,6 +1428,21 @@ static struct ctl_table vm_table[] = {
 		.extra2		= &one_hundred,
 #endif
 	},
+#ifdef CONFIG_OPLUS_MM_HACKS
+	{
+		.procname	= "direct_swappiness",
+		.data		= &direct_vm_swappiness,
+		.maxlen 	= sizeof(direct_vm_swappiness),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1 	= &zero,
+#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
+		.extra2		= &max_swappiness,
+#else
+		.extra2		= &one_hundred,
+#endif
+	},
+#endif /*CONFIG_OPLUS_MM_HACKS*/
 	{
 		.procname	= "mmap_readaround_limit",
 		.data		= &mmap_readaround_limit,
