4008e175fee5dcd0ce48bb5161dc143e9bf1d2cc
ghazzor <127341071+ghazzor@users.noreply.github.com>
2024-01-21 14:56:38 +0530

    Fix Init Script(again)

--- init_clang_12.sh
+++ init_clang_12.sh
@@ -1,10 +1,10 @@
 #clone proton clang v12 toolchain
 wget https://github.com/kdrag0n/proton-clang/archive/refs/tags/20210123.zip
-unzip proton-clang-20210123.zip
-mv proton-clang-20210123 proton-clang
+unzip 20210123.zip
+mv 20210123 proton-clang
+mv proton-clang toolchain
 #gcc 12.3 
-wget -r -np -N https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
-mv armkeil.blob.core.windows.net/developer/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
 tar -xf arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
 mv arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu gcc
 mv gcc toolchain
