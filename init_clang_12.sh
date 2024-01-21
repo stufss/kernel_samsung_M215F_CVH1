33025cc2ca75762ce52d78a9bb948987e605ee61
ghazzor <127341071+ghazzor@users.noreply.github.com>
2024-01-21 15:05:25 +0530

    Update init_clang_12.sh

--- init_clang_12.sh
+++ init_clang_12.sh
@@ -2,7 +2,7 @@
 mkdir -p toolchain
 wget https://github.com/kdrag0n/proton-clang/archive/refs/tags/20210123.zip
 unzip 20210123.zip
-mv 20210123 proton-clang
+mv proton-clang-20210123 proton-clang
 mv proton-clang toolchain
 #gcc 12.3 
 wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
