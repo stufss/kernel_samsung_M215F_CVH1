ed6fd53c8834a2fdd5e38e8db5cbaf535f639cb4
ghazzor <kartikhis8o@gmail.com>
2024-01-22 03:13:24 +0530

    Adapt Build & Init Scripts 	modified:   auto_workflow_clang.sh 	modified:   init_clang_12.sh

--- auto_workflow_clang.sh
+++ auto_workflow_clang.sh
@@ -1,19 +1,8 @@
 export LD_LIBRARY_PATH="$PWD/toolchain/proton-clang/lib:$LD_LIBRARY_PATH"
-export CROSS_COMPILE=$PWD/toolchain/gcc/bin/aarch64-none-linux-gnu-
-export CLANG_TRIPLE=$PWD/toolchain/proton-clang/bin/aarch64-linux-gnu-
-export CC=clang
-export LD=ld.lld
-export AR=llvm-ar
-export NM=llvm-nm
-export OBJCOPY=llvm-objcopy
-export OBJDUMP=llvm-objdump
-export READELF=llvm-readelf
-export OBJSIZE=llvm-size
-export STRIP=llvm-strip
-export LLVM_AR=llvm-ar
-export LLVM_DIS=llvm-dis
-export LLVM_NM=llvm-nm
-
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export LLVM=1
 
 export ARCH=arm64
 export PLATFORM_VERSION=12
@@ -21,8 +10,20 @@ export ANDROID_MAJOR_VERSION=s
 
 make clean && make distclean
 
-make CC=clang LD=ld.lld ARCH=arm64 KCFLAGS=-w AR=llvm-ar NM=llvm-nm LLVM_NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP=llvm-strip LLVM_AR=llvm-ar LLVM_DIS=llvm-dis CONFIG_SECTION_MISMATCH_WARN_ONLY=y exynos9610-m21dd_defconfig
-make CC=clang LD=ld.lld ARCH=arm64 KCFLAGS=-w AR=llvm-ar NM=llvm-nm LLVM_NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP=llvm-strip LLVM_AR=llvm-ar LLVM_DIS=llvm-dis CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
+ARGS='
+CC=clang
+LD=ld.lld
+ARCH=arm64
+LLVM=1
+CROSS_COMPILE=aarch64-linux-gnu-
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+CLANG_TRIPLE=aarch64-linux-gnu-
+'
+
+make clean && make distclean
+make ${ARGS} KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y exynos9610-m21dd_defconfig
+make ${ARGS} KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
+
 
 echo "Cleaning Stuff"
 rm -rf AIK/Image
--- init_clang_12.sh
+++ init_clang_12.sh
@@ -1,8 +1,8 @@
 #clone proton clang v12 toolchain
-git clone --depth=1 --branch 20210123 https://github.com/kdrag0n/proton-clang.git toolchain/proton-clang
+git clone --depth=1 -j$(nproc) https://github.com/ghazzor/proton-clang -b main toolchain/proton-clang
 
 #gcc 12.3 
-wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
-tar -xf arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
-mv arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu gcc
-mv gcc toolchain
+#wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+#tar -xf arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+#mv arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu gcc
+#mv gcc toolchain
