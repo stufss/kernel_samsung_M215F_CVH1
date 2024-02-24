f9727d9a8e20457d5e35f5eeaada3890cdd8e19a
ghazzor <kartikhis8o@gmail.com>
2024-02-24 05:41:29 +0530

    Clean Trees

--- init_clang_12.sh
+++
@@ -1,8 +0,0 @@
-#clone proton clang v12 toolchain
-git clone --depth=1 https://github.com/ghazzor/proton-12 toolchain/proton-clang
-
-#gcc 12.3 
-#wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
-#tar -xf arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
-#mv arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu gcc
-#mv gcc toolchain
