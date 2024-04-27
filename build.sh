b11bb369b4602ffa138d39bb4099ddec43411ff6
ghazzor <kartikhis8o@gmail.com>
2024-04-27 17:18:04 +0530

    buildscript: re-adapt to ksu
    Signed-off-by: ghazzor <kartikhis8o@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+[ ! -e "KernelSU/kernel/setup.sh" ] && git submodule init && git submodule update
+
+export KBUILD_BUILD_USER=ghazzor
+
 PATH=$PWD/toolchain/bin:$PATH
 export LLVM_DIR=$PWD/toolchain/bin
 export LLVM=1
@@ -9,6 +13,18 @@ export ANDROID_MAJOR_VERSION=s
 if [ -z "$DEVICE" ]; then
 export DEVICE=m21
 fi
+
+if [[ -z "$KSU" || "$KSU" = "0" ]]; then
+KSU=0
+export KSUSTAT=-
+elif [ "$KSU" = "1" ]; then
+CONFIG_KSU=ksu.config
+export KSUSTAT=_KSU-
+else
+echo "Error: Set KSU to 0 or 1 to build"
+exit 1
+fi
+export KSU
 
 if [[ -z "$CB" || "$CB" = "y" ]]; then
 echo "Clean Build"
@@ -41,7 +57,7 @@ LLVM=1
 '
 
 clear
-make ${ARGS} O=out ${DEVICE}_defconfig naz.config
+make ${ARGS} O=out ${DEVICE}_defconfig naz.config ${CONFIG_KSU}
 make ${ARGS} O=out -j$(nproc)
 
 echo "  Cleaning Stuff"
@@ -83,7 +99,7 @@ kmod=$(echo ${kver} | awk -F'.' '{print $3}')
 echo "  Zipping Stuff"
 cd AnyKernel3
 rm -rf N_KERNEL.*.zip
-zip -r1 N_KERNEL.${kmod}_${DEVICE}-${TIME}.zip * -x .git README.md *placeholder
+zip -r1 N_KERNEL.${kmod}_${DEVICE}${KSUSTAT}${TIME}.zip * -x .git README.md *placeholder
 cd ..
 
 if [[ -z "$AUTO" || "$AUTO" = "0" ]]; then
