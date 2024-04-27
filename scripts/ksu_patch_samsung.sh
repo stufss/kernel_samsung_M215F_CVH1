4d65a79a94a0e54e86897084eaea56300bc21cce
ghazzor <kartikhis8o@gmail.com>
2024-04-27 17:47:26 +0530

    scripts: fix ksu patch script

--- scripts/ksu_patch_samsung.sh
+++ scripts/ksu_patch_samsung.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define Search Directory
-search_dir="drivers/kernelsu"
+search_dir="KernelSU/kernel"
 
 # Find all in the files and replace.
 grep -rl 'if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)' "$search_dir" | xargs sed -i 's/if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)/if 1/g'
