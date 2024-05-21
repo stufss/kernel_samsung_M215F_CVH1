b7e16dff41bdf21e6620418874c766e784bf3df1
ghazzor <kartikhis8o@gmail.com>
2024-05-21 15:29:38 +0530

    initscript: Switch to clang 18

--- init_clang.sh
+++ init_clang.sh
@@ -1,7 +1,7 @@
 mkdir -p toolchain
 cd toolchain
 echo 'Download antman and sync'
-bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
+bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=05012024
 echo 'Patch for glibc'
 bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
 echo 'Done'
