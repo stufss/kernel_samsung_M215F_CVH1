ad10246351f0c248228e8b197ac7c30f56abffaf
ghazzor <127341071+ghazzor@users.noreply.github.com>
2024-05-30 02:46:25 +0530

    arm64:fpsimd.c: kang from grass

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -247,9 +247,13 @@ void fpsimd_restore_current_state(void)
 	 * FPSTATE is clean (as we can't have one) to avoid looping for ever in
 	 * do_notify_resume().
 	 */
-	if (!system_supports_fpsimd())
+	if (!system_supports_fpsimd()) {
+		clear_thread_flag(TIF_FOREIGN_FPSTATE);
 		return;
+	}
+
 	preempt_disable();
+
 	if (test_and_clear_thread_flag(TIF_FOREIGN_FPSTATE)) {
 		struct fpsimd_state *st = &current->thread.fpsimd_state;
 
