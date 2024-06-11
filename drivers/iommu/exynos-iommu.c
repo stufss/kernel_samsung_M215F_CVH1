ce5b8cc614645ca3c0388c2d23db8e5912d4e1dc
ghazzor <kartikhis8o@gmail.com>
2024-06-11 15:44:16 +0530

    drivers/iommu/exynos-iommu.c: fix

--- drivers/iommu/exynos-iommu.c
+++ drivers/iommu/exynos-iommu.c
@@ -577,7 +577,7 @@ static int __init exynos_sysmmu_probe(struct platform_device *pdev)
 	ret = iommu_device_register(&data->iommu);
 	if (ret) {
 		dev_err(dev, "Failed to register device\n");
-		return ret;
+		goto err_iommu_register;
 	}
 
 	pm_runtime_enable(dev);
@@ -1235,15 +1235,12 @@ static int exynos_iommu_of_xlate(struct device *master,
 	data = platform_get_drvdata(sysmmu_pdev);
 	if (!data)
 		return -ENODEV;
-	}
 
 	sysmmu = data->sysmmu;
 	if (!owner) {
 		owner = kzalloc(sizeof(*owner), GFP_KERNEL);
-		if (!owner) {
-			put_device(&sysmmu->dev);
+		if (!owner)
 			return -ENOMEM;
-		}
 
 		INIT_LIST_HEAD(&owner->sysmmu_list);
 		INIT_LIST_HEAD(&owner->client);
